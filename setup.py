bc351062ccbf4b47c60039f5badda3581e382f90
Tyler Stegmaier <jakar510@gmail.com>
2020-01-13 12:15:28 -0600

    initial commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from src.PySwitchCase.__version__ import __version__
+
+
+setup(
+        name='PySwitchCase',
+        version=__version__,
+        packages=[''],
+        url='',
+        license='Proprietary',
+        author='Tyler Stegmaier',
+        author_email='',
+        description='A pure python way to efficiently do a c++ style switch case in Python 3.6+.',
+        install_requires=[]
+        )
+
