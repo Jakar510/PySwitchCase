7b45d01a88cf6ebfa02096445f0a8651b73cf092
Tyler Stegmaier <jakar510@gmail.com>
2020-01-15 13:04:26 -0600

    1.0.0
    • updated setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,47 @@
 from setuptools import setup
-from src.PySwitchCase.__version__ import __version__
+from src.PySwitchCase.__version__ import version
 
 
+
+
+data_files = [
+        'PySwitchCase/*.py'
+        ]
 setup(
         name='PySwitchCase',
-        version=__version__,
-        packages=[''],
+        version=version,
+        packages=['PySwitchCase'],
         url='https://github.com/Jakar510/PySwitchCase',
-        license='Proprietary',
+        license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
         author='Tyler Stegmaier',
-        author_email='',
+        author_email='tyler.stegmaier.510@gmail.com',
         description='A pure python way to efficiently do a c++ style switch case in Python 3.6+.',
-        install_requires=[]
+        install_requires=[],
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
+            # Pick your license as you wish
+            'License :: Free To Use But Restricted',
+
+            # Support platforms
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+
+            'Programming Language :: Python :: 3',
+        ],
+        keywords='switch switch-case case',
+        package_dir={'PySwitchCase': 'src/PySwitchCase'},
+        package_data={
+                'PySwitchCase': data_files,
+            },
         )
 
